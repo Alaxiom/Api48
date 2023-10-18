1adf9c99d7a92b27b52fd482b4e9376d8293c84a
Alan Every <alan.every@cantab.net>
2023-10-18 13:10:37 +0100

    Add web client

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -1,15 +1,17 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace IdentityServer;
 
 public static class Config
 {
     public static IEnumerable<IdentityResource> IdentityResources =>
-        new IdentityResource[]
-        { 
-            new IdentityResources.OpenId()
-        };
-
+    new List<IdentityResource>
+    {
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile(),
+    };
+    
     public static IEnumerable<ApiScope> ApiScopes =>
     new List<ApiScope>
     {
@@ -36,6 +38,25 @@ public static class Config
 
             // scopes that client has access to
             AllowedScopes = { "api1", "api2" }
+        },
+        new Client
+        {
+            ClientId = "web",
+            ClientSecrets = { new Secret("secret".Sha256()) },
+
+            AllowedGrantTypes = GrantTypes.Code,
+            
+            // where to redirect to after login
+            RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+            // where to redirect to after logout
+            PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+            AllowedScopes = new List<string>
+            {
+                IdentityServerConstants.StandardScopes.OpenId,
+                IdentityServerConstants.StandardScopes.Profile              
+            }
         }
     };
 
